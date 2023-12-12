db060ff1f0b840f40099279732a923c94a18b410
gyuni1337 <silviplosakov@gmail.com>
2023-12-12 11:02:13 +0100

    changes to responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,17 @@ export default async function Index() {
     <>
       <Navbar />
 
-      <div className="flex lg:flex-row flex-col lg:mt-[8%] mt-[30%] gap-2 lg:mx-[20%] ">
+      <div className="flex lg:flex-row flex-col items-center lg:mt-[8%] mt-[30%] lg:gap-16 lg:mx-[20%] ">
 
       <div className='flex gap-32 items-center'>
-        <div className='flex flex-col items-center lg:items-start lg:justify-center gap-5'>
+        <div className='flex flex-col items-center justify-center lg:items-start lg:justify-center gap-5'>
         <h1 className='text-4xl text-red-400'>Sticker Heaven </h1>
         <p className='text-lg lg:max-w-lg '>Your Sticker Paradise! Explore and collect a variety of stickers, from timeless classics to exclusive limited editions. Start your sticker journey today!</p>
         </div>
+      </div>
 
         <Image alt='logo' src='/logo.png' width={400} height={400} />
 
-      </div>
 
       </div>
         
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,50 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Navbar() {
+
+  const [visible, makeVisible] = useState(false);
+  
+  function showMenu() {
+    makeVisible(!visible);
+
+  }
+
   return (
     <>
-    <div className='flex justify-around mt-10 lg:mx-32 items-center'>
-          <div className='flex items-center gap-2'>
+    <div className='flex flex-col justify-around mt-10 lg:mx-32'>
+      <div className='flex flex-row justify-around w-full items-center'>
+
+          <div className='flex flex-row items-center gap-2'>
           <Link href="/" className='text-xl mt-1.5'>stickerheaven</Link>
         <h1 className='animate-bounce text-red-400'>&lt;3</h1>
           </div>
 
           <div className='lg:flex gap-10 hidden'>
-          <Link href="/trading" className='text-bg cursor-pointer text-lg mt-1.5'>trading</Link>
+          <Link href="/market" className='text-bg cursor-pointer text-lg mt-1.5'>market</Link>
           <Link href="/stickers" className='text-bg cursor-pointer text-lg mt-1.5'>stickers</Link>
           <Link href="/cases" className='text-bg cursor-pointer text-lg mt-1.5'>cases</Link>
           <Link href="/login" className='bg-red-400 p-2 rounded-xl px-5 text-white border-1 border-white hover:bg-red-500 transition-colors duration-300 shadow cursor-pointer' >Sign In</Link>
 
           </div>
 
+
         <div className='lg:hidden block'>
-          <AiOutlineMenu className='w-12 h-12' />
-        </div> 
+          <AiOutlineMenu className='w-12 h-12' onClick={showMenu} />
+          </div>  
+
+      </div>
+          <div className={`${visible ? "flex" : "hidden"} flex-col mx-12 gap-5 mt-5`}>
+            <Link href="/market" className='text-bg cursor-pointer text-lg mt-1.5'>market</Link>
+            <Link href="/stickers" className='text-bg cursor-pointer text-lg mt-1.5'>stickers</Link>
+            <Link href="/cases" className='text-bg cursor-pointer text-lg mt-1.5'>cases</Link>
+            <Link href="/login" className='bg-red-400 p-2 rounded-xl px-5 text-white border-1 border-white hover:bg-red-500 transition-colors duration-300 shadow cursor-pointer' >Sign In</Link>
+
+    </div>
 
       </div>
     </>
