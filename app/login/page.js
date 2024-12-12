fa93e065addede3807e657fe26123d7bebc7e24a
gyuni1337 <silviplosakov@gmail.com>
2024-12-12 22:43:24 +0100

    began working on the login page, and swapped the main page to index instead of login

---
+++ app/login/page.js
@@ -0,0 +1,37 @@
+'use client'
+
+import '../globals.css';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Index() {
+  const router = useRouter();
+  
+  let [email, setEmail ] = useState('');
+  let [password, setPassword ] = useState('');
+  let [error, setError ] = useState('');
+
+
+  const handleLogin = async () => {
+        router.push('/main');        
+  }
+
+   return (
+    <>
+    <div className='text-xl flex mx-auto w-[400px] mt-10 flex-col'>
+      <h2 className='text-center mb-5'>LOGIN</h2>
+      <div className='flex flex-col'>
+        <label htmlFor='username'>Email</label>
+        <input onChange={(e) => setEmail(e.target.value)} className='border-2 border-green-300' type='text' id='username' />
+      </div>
+      <div className='flex flex-col'>
+        <label htmlFor='username'>Password</label>
+        <input onChange={(e) => setPassword(e.target.value)} className='border-2 border-green-300' type='text' id='username' />
+      </div>
+      <button onClick={handleLogin} className='bg-green-300 mt-10'>Login</button>
+      {error}
+    </div>
+    </>
+  )
+}
+
--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,66 @@
-'use client'
 
-import './globals.css'
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-export default function Index() {
-  const router = useRouter();
-  
-  let [email, setEmail ] = useState('');
-  let [password, setPassword ] = useState('');
-  let [error, setError ] = useState('');
+"use client"
+// import { createClient } from '@/utils/supabase/server';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Image from 'next/image';
+import { use, useEffect, useState } from 'react';
+import db from './utils/db';
+import { useRouter } from 'next/navigation'
 
 
-  const handleLogin = async () => {
-        router.push('/main');        
+export default function Page() {
+  const router = useRouter()
+
+    let [limited, setLimited] = useState([]);
+
+  useEffect(() => {
+    db.get('/v1/stickers/limited')
+    .then((response) => {
+      setLimited(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
+  , [])
 
-   return (
+
+  return (
     <>
-    <div className='text-xl flex mx-auto w-[400px] mt-10 flex-col'>
-      <h2 className='text-center mb-5'>LOGIN</h2>
-      <div className='flex flex-col'>
-        <label htmlFor='username'>Email</label>
-        <input onChange={(e) => setEmail(e.target.value)} className='border-2 border-green-300' type='text' id='username' />
+      <Navbar />
+
+      <div className="flex lg:flex-row flex-col items-center lg:mt-[8%] mt-[30%] lg:gap-16 lg:mx-[20%] ">
+
+      <div className='flex gap-32 items-center'>
+        <div className='flex flex-col items-center justify-center lg:items-start lg:justify-center gap-5'>
+        <h1 className='text-4xl text-red-400'>Sticker Heaven </h1>
+        <p className='text-lg lg:max-w-lg '>Your Sticker Paradise! Explore and collect a variety of stickers, from timeless classics to exclusive limited editions. Start your sticker journey today!</p>
+        </div>
       </div>
-      <div className='flex flex-col'>
-        <label htmlFor='username'>Password</label>
-        <input onChange={(e) => setPassword(e.target.value)} className='border-2 border-green-300' type='text' id='username' />
+
+        <Image alt='logo' src='/logo.png' width={400} height={400} />
+      
       </div>
-      <button onClick={handleLogin} className='bg-green-300 mt-10'>Login</button>
-      {error}
-    </div>
+        
+        <h2 className='text-3xl text-red-400 flex justify-center lg:mt-[15%] mt-[70%] '>Limited Stickers</h2>
+
+      <div className='flex justify-center mt-[5%] mb-[10%]'>
+
+        <div className='flex justify-around items-center flex-wrap max-w-5xl gap-7'>
+          {
+            limited && limited.map((current) => {
+              return (
+                <Card key={current.id} title={current.title} description={current.description} img={current.img} price={current.price} />
+              )
+            })
+          }
+
+        </div>
+      </div>
     </>
+    
+
   )
 }
+
